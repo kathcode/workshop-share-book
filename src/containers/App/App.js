cc6ced880d9bc8284866b48d583eabab497e256c
Kath_code <kathmene27@gmail.com>
2020-06-15 17:38:04 -0500

    [Step1] - Setup the application

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Components
+import Header from '../../shared/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="ShareBook" />
+    </div>
+  );
+}
+
+export default App;
