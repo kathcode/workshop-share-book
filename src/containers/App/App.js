a1380f5f59dc143a35a172f46ef80d70de4e0c2f
Kath_code <kathmene27@gmail.com>
2020-06-15 18:25:20 -0500

    [Step 1] - Create the container component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
-// Components
+// Shared Components
 import Header from '../../shared/Header';
+import Container from '../../shared/Container';
 
 function App() {
   return (
     <div className="App">
       <Header title="ShareBook" />
+      <Container>
+        <div>Container...</div>
+      </Container>
     </div>
   );
 }
