6b38c38e5f70d6f5086c7533a315a9c63cc16516
Kath_code <kathmene27@gmail.com>
2020-06-16 21:39:35 -0500

    [Step4] - Add the create component.

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 import './Create.css';
 
@@ -8,20 +8,61 @@ import Header from '../../shared/Header';
 import Container from '../../shared/Container';
 import FloatingIcon from '../../shared/FloatingIcon';
 
-const Create = (porps) => {
-  return (
-    <div>
-      <Header title="ShareBook" />
-      <Link to="/">
-        <FloatingIcon className="close-button" icon="x" />
-      </Link>
-      <Container>
-        <div>Create...</div>
-      </Container>
-    </div>
-  );
-};
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
 
-Create.propTypes = {};
+    this.state = {
+      author: '',
+      imageUrl: '',
+    };
+  }
+
+  handleOnSave = () => {
+    const { onCreate } = this.props;
+    onCreate(this.state);
+  };
+
+  handleOnUpdateField = (value, field) => {
+    this.setState({
+      [field]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="ShareBook" />
+        <Link to="/">
+          <FloatingIcon className="close-button" icon="x" />
+        </Link>
+        <Container>
+          <div className="form">
+            <h2>Create new book</h2>
+            <input
+              onChange={(event) =>
+                this.handleOnUpdateField(event.target.value, 'author')
+              }
+              placeholder="Add the author"
+            />
+            <input
+              onChange={(event) =>
+                this.handleOnUpdateField(event.target.value, 'imageUrl')
+              }
+              placeholder="Add the image url of the book"
+            />
+            <Link to="/" onClick={this.handleOnSave}>
+              <button>Create</button>
+            </Link>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+Create.propTypes = {
+  onCreate: PropTypes.func,
+};
 
 export default Create;
